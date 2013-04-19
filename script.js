5724428012c185417a541c165df88a0a6d80db29
Daniel Golden <danielgolden90@gmail.com>
2013-04-19 16:00:06 -0400

    Adds ability to toggle completed tasks

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 $('li:contains("@done"), h1:contains("@done"), h2:contains("@done"), h3:contains("@done"), h4:contains("@done"), h5:contains("@done"), h5:contains("@done")').each(function() {
   $(this).addClass('completed_task');
 });
+
+$('.doctitle').click(function() {
+	if($('.completed_tasks_hidden').length > 0) {
+		$(this).removeClass('completed_tasks_hidden');
+		$('.completed_task').show();
+	} else {
+		$(this).addClass('completed_tasks_hidden');
+		$('.completed_task').hide();
+	}
+});

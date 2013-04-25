91f867402c6744c6d10e56a53947a16f6b6aa57c
Daniel Golden <danielgolden90@gmail.com>
2013-04-25 17:00:59 -0400

    Adds styles for .note and js to make it happen

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $('li:contains("@done"), h1:contains("@done"), h2:contains("@done"), h3:contains("@done"), h4:contains("@done"), h5:contains("@done"), h5:contains("@done")').each(function() {
-  $(this).addClass('completed_task');
+	$(this).addClass('completed_task');
+});
+
+$('p:contains("@note")').each(function() {
+	$(this).addClass('note');
 });
 
 $('.doctitle').click(function() {
